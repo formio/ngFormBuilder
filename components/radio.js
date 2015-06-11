0818a7e581f08762ffdee5d6f4eebec94b1f03d2
Travis Tidwell <codertravist@gmail.com>
2015-06-11 09:25:10 -0500

    Revert "Refactored radio value builder into directive"
    This reverts commit c957869da2f49474a980d8a016ec8add370710f2.

--- components/radio.js
+++ components/radio.js
@@ -5,7 +5,22 @@ app.config([
       views: [
         {
           name: 'Display',
-          template: 'formio/components/radio/display.html'
+          template: 'formio/components/radio/display.html',
+          controller: [
+            '$scope',
+            function($scope) {
+              $scope.addValue = function() {
+                var value = $scope.component.values.length + 1;
+                $scope.component.values.push({
+                  value: 'value' + value,
+                  label: 'Value ' + value
+                });
+              };
+              $scope.removeValue = function(index) {
+                $scope.component.values.splice(index, 1);
+              };
+            }
+          ]
         },
         {
           name: 'Validation',
@@ -25,9 +40,28 @@ app.run([
 
     // Create the settings markup.
     $templateCache.put('formio/components/radio/display.html',
-      '<ng-form>' +
+      '<ng-form ng-controller="view.controller">' +
         '<form-builder-option property="label"></form-builder-option>' +
-        '<value-builder data="component.values" label="Values" tooltip-text="The radio button values that can be picked for this field. Values are text submitted with the form data. Labels are text that appears next to the radio buttons on the form."></value-builder>' +
+        '<div class="form-group">' +
+          '<label for="" form-builder-tooltip="The radio button values that can be picked for this field. Values are text submitted with the form data. Labels are text that appears next to the radio buttons on the form.">Values</label>' +
+          '<table class="table table-condensed">' +
+            '<thead>' +
+              '<tr>' +
+                '<th class="col-xs-4">Value</th>' +
+                '<th class="col-xs-6">Label</th>' +
+                '<th class="col-xs-2"></th>' +
+              '</tr>' +
+            '</thead>' +
+            '<tbody>' +
+              '<tr ng-repeat="v in component.values track by $index">' +
+                '<td class="col-xs-4"><input type="text" class="form-control" ng-model="v.value"/></td>' +
+                '<td class="col-xs-6"><input type="text" class="form-control" ng-model="v.label"/></td>' +
+                '<td class="col-xs-2"><button class="btn btn-danger btn-xs" ng-click="removeValue($index)"><span class="glyphicon glyphicon-remove-circle"></span></button></td>' +
+              '</tr>' +
+            '</tbody>' +
+          '</table>' +
+          '<button class="btn" ng-click="addValue()">Add Value</button>' +
+        '</div>' +
         '<form-builder-option property="protected"></form-builder-option>' +
         '<form-builder-option property="persistent"></form-builder-option>' +
       '</ng-form>'
