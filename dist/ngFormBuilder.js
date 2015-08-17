6ef6412336f2f57a9daf61b79eb12e6c58f4b95c
Rahat Ahmed <rahatarmanahmed@gmail.com>
2015-08-17 14:06:57 -0500

    Updated build

--- dist/ngFormBuilder.js
+++ dist/ngFormBuilder.js
@@ -436,14 +436,6 @@ app.constant('FORM_OPTIONS', {
       title: 'Submit'
     },
     {
-      name: 'prevPage',
-      title: 'Previous Page'
-    },
-    {
-      name: 'nextPage',
-      title: 'Next Page'
-    },
-    {
       name: 'reset',
       title: 'Reset'
     }
