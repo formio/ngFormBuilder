fcc70be2308e20c8a423a9137a7e5edebfdfa05b
Rahat Ahmed <rahatarmanahmed@gmail.com>
2015-08-24 13:13:01 -0500

    FA-287 Disabled dragging password inputs
    There's an issue with angular-drag-and-drop-lists with password fields where it will start the drag operation but never call drag end or anything to stop the operation. Likely a security measure for passwords. This gets the dndDragging/dndDraggingSource classes stuck which hide the component. I added dnd-nodrag to password fields so they can't be dragged.

--- dist/ngFormBuilder.js
+++ dist/ngFormBuilder.js
@@ -1458,8 +1458,23 @@ app.config([
   function(formioComponentsProvider) {
     formioComponentsProvider.register('password', {
       views: formioComponentsProvider.$get().components.textfield.views,
-      documentation: 'http://help.form.io/userguide/#password'
+      documentation: 'http://help.form.io/userguide/#password',
+      template: 'formio/components/password.html'
     });
+  }
+]);
+app.run([
+  '$templateCache',
+  function(
+    $templateCache
+  ) {
+    // Disable dragging on password inputs because it breaks dndLists
+    var textFieldTmpl = $templateCache.get('formio/components/textfield.html');
+    var passwordTmpl = textFieldTmpl.replace(
+      /<input type="{{ component.inputType }}" /g,
+      '<input type="{{ component.inputType }}" dnd-nodrag '
+    );
+    $templateCache.put('formio/components/password.html', passwordTmpl);
   }
 ]);
 
