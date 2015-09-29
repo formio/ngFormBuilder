e28cb8a25ba62b9a267f5bc921f18670d90d3b1e
Travis Tidwell <codertravist@gmail.com>
2015-09-28 22:15:39 -0500

    Updating the form builder to include styles.

--- dist/ngFormBuilder.js
+++ dist/ngFormBuilder.js
@@ -1,5 +1,7 @@
 (function () {
 'use strict';
+/*global window: false, console: false, $: false */
+/*jshint browser: true */
 var app = angular.module('ngFormBuilder', [
   'formio',
   'dndLists',
@@ -12,8 +14,7 @@ app.directive('formBuilder', ['debounce', function(debounce) {
     replace: true,
     templateUrl: 'formio/formbuilder/builder.html',
     scope: {
-      project: '=',
-      form: '='
+      src: '='
     },
     controller: [
       '$scope',
@@ -29,7 +30,20 @@ app.directive('formBuilder', ['debounce', function(debounce) {
         FormioUtils
       ) {
         // Add the components to the scope.
-        $scope.formio = new Formio('/project/' + $scope.project);
+        var submitButton = angular.copy(formioComponents.components.button.settings);
+        $scope.form = {components:[submitButton]};
+        $scope.formio = new Formio($scope.src);
+
+        // Load the form.
+        if ($scope.formio.formId) {
+          $scope.formio.loadForm().then(function(form) {
+            $scope.form = form;
+            if ($scope.form.components.length === 0) {
+              $scope.form.components.push(submitButton);
+            }
+          });
+        }
+
         $scope.formComponents = _.cloneDeep(formioComponents.components);
         _.each($scope.formComponents, function(component) {
           component.settings.isNew = true;
@@ -38,11 +52,6 @@ app.directive('formBuilder', ['debounce', function(debounce) {
         $scope.formComponentsByGroup = _.groupBy($scope.formComponents, function(component) {
           return component.group;
         });
-
-        // Add the submit button to the components.
-        if ($scope.form.components.length === 0) {
-          $scope.form.components.push(angular.copy(formioComponents.components.button.settings));
-        }
 
         // Get the resource fields.
         $scope.formio.loadForms({params: {type: 'resource'}}).then(function(resources) {
@@ -152,7 +161,7 @@ app.directive('formBuilder', ['debounce', function(debounce) {
           }
 
           $scope.$broadcast('ckeditor.refresh');
-
+          $scope.$emit('formUpdate', $scope.form);
           return component;
         };
 
@@ -221,6 +230,7 @@ app.directive('formBuilder', ['debounce', function(debounce) {
 
         $scope.saveSettings = function() {
           ngDialog.closeAll(true);
+          $scope.$emit('formUpdate', $scope.form);
         };
       }
     ],
