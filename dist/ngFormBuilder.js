f2deaa3c3b506b2be6c46ac92bbe4670e4af69ed
Randall Knutson <randall@form.io>
2015-10-22 15:09:20 -0500

    FA-472 Remove searchExpression field since we don't need it anymore.

--- dist/ngFormBuilder.js
+++ dist/ngFormBuilder.js
@@ -1732,14 +1732,13 @@ app.run([
           '<label for="placeholder" form-builder-tooltip="The resource to be used with this field.">Resource</label>' +
           '<select class="form-control" id="resource" name="resource" ng-options="value._id as value.title for value in resources" ng-model="component.resource"></select>' +
         '</div>' +
-        '<form-builder-option property="searchExpression" label="Search Expression" placeholder="The search string regular expression" title="A regular expression to filter the results with."></form-builder-option>' +
         '<div class="form-group">' +
           '<label for="placeholder" form-builder-tooltip="The properties on the resource to return as part of the options. Separate property names by commas. If left blank, all properties will be returned.">Select Fields</label>' +
           '<input type="text" class="form-control" id="selectFields" name="selectFields" ng-model="component.selectFields" placeholder="Comma separated list of fields to select." value="{{ component.selectFields }}">' +
         '</div>' +
         '<div class="form-group">' +
           '<label for="placeholder" form-builder-tooltip="A list of search filters based on the fields of the resource. See the <a target=\'_blank\' href=\'https://github.com/travist/resourcejs#filtering-the-results\'>Resource.js documentation</a> for the format of these filters.">Search Fields</label>' +
-          '<input type="text" class="form-control" id="searchFields" name="searchFields" ng-model="component.searchFields" ng-list placeholder="The search field parings" value="{{ component.searchFields }}">' +
+          '<input type="text" class="form-control" id="searchFields" name="searchFields" ng-model="component.searchFields" ng-list placeholder="The fields to query on the server" value="{{ component.searchFields }}">' +
         '</div>' +
         '<div class="form-group">' +
           '<label for="placeholder" form-builder-tooltip="The HTML template for the result data items.">Item Template</label>' +
