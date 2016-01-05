b299926fd074d04eabe16c5ea4f600b0bc6de1b3
Rahat Ahmed <rahatarmanahmed@gmail.com>
2016-01-05 13:24:20 -0600

    Added checkboxes component

---
+++ components/checkboxes.js
@@ -0,0 +1,54 @@
+app.config([
+  'formioComponentsProvider',
+  function(formioComponentsProvider) {
+    formioComponentsProvider.register('checkboxes', {
+      views: [
+        {
+          name: 'Display',
+          template: 'formio/components/checkboxes/display.html'
+        },
+        {
+          name: 'Validation',
+          template: 'formio/components/checkboxes/validate.html'
+        },
+        {
+          name: 'API',
+          template: 'formio/components/checkboxes/api.html'
+        }
+      ],
+      documentation: 'http://help.form.io/userguide/#checkboxes'
+    });
+  }
+]);
+app.run([
+  '$templateCache',
+  function($templateCache) {
+
+    // Create the settings markup.
+    $templateCache.put('formio/components/checkboxes/display.html',
+      '<ng-form>' +
+        '<form-builder-option property="label"></form-builder-option>' +
+        '<value-builder data="component.values" label="Checkboxes" tooltip-text="Checkboxes to display. Labels are shown in the form. Values are the corresponding values saved with the submission."></value-builder>' +
+        '<form-builder-option property="inline" type="checkbox" label="Inline Layout" title="Displays the checkboxes horizontally."></form-builder-option>' +
+        '<form-builder-option property="protected"></form-builder-option>' +
+        '<form-builder-option property="persistent"></form-builder-option>' +
+        '<form-builder-option property="tableView"></form-builder-option>' +
+      '</ng-form>'
+    );
+
+    // Create the API markup.
+    $templateCache.put('formio/components/checkboxes/api.html',
+      '<ng-form>' +
+        '<form-builder-option-key></form-builder-option-key>' +
+      '</ng-form>'
+    );
+
+    // Create the API markup.
+    $templateCache.put('formio/components/checkboxes/validate.html',
+      '<ng-form>' +
+        '<form-builder-option property="validate.required"></form-builder-option>' +
+        '<form-builder-option-custom-validation></form-builder-option-custom-validation>' +
+      '</ng-form>'
+    );
+  }
+]);
