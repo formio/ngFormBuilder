00b0aed8c4e1d3974589c3ff29456c9679ff812a
Travis Tidwell <travis@form.io>
2016-04-25 21:23:39 -0500

    Update form builder.

--- dist/ngFormBuilder.js
+++ dist/ngFormBuilder.js
@@ -19374,7 +19374,7 @@ module.exports = ['$timeout','$q', function($timeout, $q) {
 
 },{}],52:[function(require,module,exports){
 "use strict";
-/*! ng-formio-builder v1.11.4 | https://npmcdn.com/ng-formio-builder@1.11.4/LICENSE.txt */
+/*! ng-formio-builder v1.11.5 | https://npmcdn.com/ng-formio-builder@1.11.5/LICENSE.txt */
 /*global window: false, console: false */
 /*jshint browser: true */
 
