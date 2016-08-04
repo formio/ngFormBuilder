ce5e01b2b5895be737a14d3f9e4578df96d20b88
Travis Tidwell <travis@form.io>
2016-08-03 19:41:27 -0500

    Adding Kickbox validation settings.

--- src/components/email.js
+++ src/components/email.js
@@ -2,11 +2,43 @@ module.exports = function(app) {
   app.config([
     'formioComponentsProvider',
     function(formioComponentsProvider) {
+      var views = _.cloneDeep(formioComponentsProvider.$get().components.textfield.views);
+      _.each(views, function(view) {
+        if (view.name === 'Validation') {
+          view.template = 'formio/components/email/validate.html';
+        }
+      });
       formioComponentsProvider.register('email', {
         icon: 'fa fa-at',
-        views: formioComponentsProvider.$get().components.textfield.views,
+        views: views,
         documentation: 'http://help.form.io/userguide/#email'
       });
+    }
+  ]);
+  app.run([
+    '$templateCache',
+    function($templateCache) {
+      $templateCache.put('formio/components/email/validate.html',
+        '<ng-form>' +
+          '<form-builder-option property="validate.required"></form-builder-option>' +
+          '<form-builder-option property="unique"></form-builder-option>' +
+          '<div class="panel panel-default">' +
+            '<div class="panel-heading"><h3 class="panel-title">Kickbox</h3></div>' +
+            '<div class="panel-body">' +
+              '<p>Validate this email using the Kickbox email validation service.</p>' +
+              '<div class="checkbox">' +
+                '<label for="kickbox-enable" form-builder-tooltip="Enable Kickbox validation for this email field.">' +
+                  '<input type="checkbox" id="kickbox-enable" name="kickbox-enable" ng-model="component.kickbox.enabled"> Enable' +
+                '</label>' +
+              '</div>' +
+            '</div>' +
+          '</div>' +
+          '<form-builder-option property="validate.minLength"></form-builder-option>' +
+          '<form-builder-option property="validate.maxLength"></form-builder-option>' +
+          '<form-builder-option property="validate.pattern"></form-builder-option>' +
+          '<form-builder-option-custom-validation></form-builder-option-custom-validation>' +
+        '</ng-form>'
+      );
     }
   ]);
 };
