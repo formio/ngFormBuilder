81530cc0e3b00d4ae167ff01845428c07f90b327
Zack Urben <zackurben@gmail.com>
2017-04-28 11:25:31 -0500

    Adding json validator input box to components validation panel.

--- src/directives/formBuilderOptionCustomValidation.js
+++ src/directives/formBuilderOptionCustomValidation.js
@@ -6,28 +6,32 @@ module.exports = function() {
     restrict: 'E',
     replace: true,
     template: '' +
-      '<div class="panel panel-default" id="accordion">' +
-        '<div class="panel-heading" data-toggle="collapse" data-parent="#accordion" data-target="#validationSection">' +
-          '<span class="panel-title">Custom Validation</span>' +
-        '</div>' +
-        '<div id="validationSection" class="panel-collapse collapse in">' +
-          '<div class="panel-body">' +
-            '<textarea class="form-control" rows="5" id="custom" name="custom" ng-model="component.validate.custom" placeholder="/*** Example Code ***/\nvalid = (input === 3) ? true : \'Must be 3\';">{{ component.validate.custom }}</textarea>' +
-            '<small>' +
-              '<p>Enter custom validation code.</p>' +
-              '<p>You must assign the <strong>valid</strong> variable as either <strong>true</strong> or an error message if validation fails.</p>' +
-              '<p>The global variables <strong>input</strong>, <strong>component</strong>, and <strong>valid</strong> are provided.</p>' +
-            '</small>' +
-            '<div class="well">' +
-              '<div class="checkbox">' +
-                '<label>' +
-                  '<input type="checkbox" id="private" name="private" ng-model="component.validate.customPrivate" ng-checked="component.validate.customPrivate"> <strong>Secret Validation</strong>' +
-                '</label>' +
-              '</div>' +
-              '<p>Check this if you wish to perform the validation ONLY on the server side. This keeps your validation logic private and secret.</p>' +
-            '</div>' +
-          '</div>' +
-        '</div>' +
+      '<div>' +
+      '<uib-accordion>' +
+      '  <div uib-accordion-group heading="Custom Validation" class="panel panel-default">' +
+      '    <textarea class="form-control" rows="5" id="custom" name="custom" ng-model="component.validate.custom" placeholder="/*** Example Code ***/\nvalid = (input === 3) ? true : \'Must be 3\';">{{ component.validate.custom }}</textarea>' +
+      '    <small>' +
+      '      <p>Enter custom validation code.</p>' +
+      '      <p>You must assign the <strong>valid</strong> variable as either <strong>true</strong> or an error message if validation fails.</p>' +
+      '      <p>The global variables <strong>input</strong>, <strong>component</strong>, and <strong>valid</strong> are provided.</p>' +
+      '    </small>' +
+      '    <div class="well">' +
+      '      <div class="checkbox">' +
+      '        <label>' +
+      '          <input type="checkbox" id="private" name="private" ng-model="component.validate.customPrivate" ng-checked="component.validate.customPrivate"> <strong>Secret Validation</strong>' +
+      '        </label>' +
+      '      </div>' +
+      '      <p>Check this if you wish to perform the validation ONLY on the server side. This keeps your validation logic private and secret.</p>' +
+      '    </div>' +
+      '  </div>' +
+      '  <div uib-accordion-group heading="JSON Validation" class="panel panel-default">' +
+      '    <small>' +
+      '      <p>Execute custom validation logic with JSON and <a href="http://jsonlogic.com/">JsonLogic</a>.</p>' +
+      '      <p>Submission data is available as JsonLogic variables, with the same api key as your components.</p>' +
+      '    </small>' +
+      '    <textarea class="form-control" rows="5" id="json" name="json" ng-model="component.validate.json" placeholder=\'{ ... }\'>{{ component.validate.json }}</textarea>' +
+      '  </div>' +
+      '</uib-accordion>' +
       '</div>'
   };
 };
