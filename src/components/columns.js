3a66e5b5984f5ee194aa00653360900b22ffb8e0
douglaselee <douglaselee@comcast.net>
2017-05-04 15:24:55 -0400

    Allow any number of columns with width, offset, push and pull settings

--- src/components/columns.js
+++ src/components/columns.js
@@ -3,6 +3,14 @@ module.exports = function(app) {
     'formioComponentsProvider',
     function(formioComponentsProvider) {
       formioComponentsProvider.register('columns', {
+        onEdit: ['$scope', function($scope) {
+          $scope.removeColumn = function(index) {
+            $scope.component.columns.splice(index, 1);
+          };
+          $scope.addColumn = function() {
+            $scope.component.columns.push({components: [], width: 1, offset: 0, push: 0, pull: 0});
+          };
+        }],
         fbtemplate: 'formio/formbuilder/columns.html',
         icon: 'fa fa-columns',
         documentation: 'http://help.form.io/userguide/#columns',
@@ -30,14 +38,40 @@ module.exports = function(app) {
     function($templateCache) {
       $templateCache.put('formio/formbuilder/columns.html',
         '<div class="row">' +
-          '<div class="col-xs-6 component-form-group" ng-repeat="component in component.columns">' +
-            '<form-builder-list class="formio-column" component="component" form="form" formio="::formio"></form-builder-list>' +
+          '<div ng-show="column.width" ng-class="\'col-xs-\' + column.width + \' col-xs-offset-\' + column.offset + \' col-xs-push-\' + column.push + \' col-xs-pull-\' + column.pull" component-form-group" ng-repeat="column in component.columns">' +
+            '<form-builder-list class="formio-column" component="column" form="form" formio="::formio"></form-builder-list>' +
           '</div>' +
         '</div>'
       );
       $templateCache.put('formio/components/columns/display.html',
         '<ng-form>' +
           '<form-builder-option property="customClass"></form-builder-option>' +
+          '<div class="form-group">' +
+            '<label form-builder-tooltip="The width, offset, push and pull settings for the columns">Column Properties</label>' +
+            '<table class="table table-condensed">' +
+              '<thead>' +
+                '<tr>' +
+                  '<th class="col-xs-2">Column</th>' +
+                  '<th class="col-xs-2">Width</th>' +
+                  '<th class="col-xs-2">Offset</th>' +
+                  '<th class="col-xs-2">Push</th>' +
+                  '<th class="col-xs-2">Pull</th>' +
+                  '<th class="col-xs-1"></th>' +
+                '</tr>' +
+              '</thead>' +
+              '<tbody>' +
+                '<tr ng-repeat="column in component.columns track by $index">' +
+                  '<td class="col-xs-2"><input type="number" class="form-control" ng-value="$index + 1" disabled/></td>' +
+                  '<td class="col-xs-2"><input type="number" class="form-control" min="0" max="12" ng-model="column.width"/></td>' +
+                  '<td class="col-xs-2"><input type="number" class="form-control" min="0" max="12" ng-model="column.offset"/></td>' +
+                  '<td class="col-xs-2"><input type="number" class="form-control" min="0" max="12" ng-model="column.push"/></td>' +
+                  '<td class="col-xs-2"><input type="number" class="form-control" min="0" max="12" ng-model="column.pull"/></td>' +
+                  '<td class="col-xs-1"><button type="button" class="btn btn-danger btn-xs" ng-click="removeColumn($index)" tabindex="-1"><span class="glyphicon glyphicon-remove-circle"></span></button></td>' +
+                '</tr>' +
+              '</tbody>' +
+            '</table>' +
+            '<button type="button" class="btn" ng-click="addColumn()">Add Column</button>' +
+          '</div>' +
         '</ng-form>'
       );
     }
