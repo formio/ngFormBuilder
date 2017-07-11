64b45f69029261aeb0e386333e1aee38088880fb
Edwin Anciani <edwinanciani@gmail.com>
2017-07-11 13:17:06 -0500

    WYSIWYG display bug fixed

--- src/ngFormBuilder-complete.js
+++ src/ngFormBuilder-complete.js
@@ -1,5 +1,6 @@
 require('ng-formio/src/formio-complete.js');
 require('angular-drag-and-drop-lists');
+require('../bower_components/ckeditor/ckeditor');
 require('../bower_components/angular-ckeditor/angular-ckeditor');
 require('ng-dialog');
 require('./ngFormBuilder.js');
