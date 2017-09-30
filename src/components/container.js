8e0911809140a906f92cff35c69beeb7c3525a67
Yury Rybak <yury.rybak@softensity.com>
2017-09-30 21:13:53 +0300

    Added 'encrypt' setting to components

--- src/components/container.js
+++ src/components/container.js
@@ -36,6 +36,7 @@ module.exports = function(app) {
         '<form-builder-option property="clearOnHide"></form-builder-option>' +
         '<form-builder-option property="protected"></form-builder-option>' +
         '<form-builder-option property="persistent"></form-builder-option>' +
+        '<form-builder-option property="encrypted"></form-builder-option>' +
         '<form-builder-option property="tableView"></form-builder-option>' +
         '</ng-form>'
       );
