8908f742c25117d078511ab65c998c21aa97f3c2
AlenaLevina <alenalevina1@gmail.com>
2018-02-13 13:05:57 +0300

    Columns component: Added ability to specify label

--- src/components/columns.js
+++ src/components/columns.js
@@ -45,6 +45,7 @@ module.exports = function(app) {
       );
       $templateCache.put('formio/components/columns/display.html',
         '<ng-form>' +
+          '<form-builder-option property="label"></form-builder-option>' +
           '<form-builder-option property="customClass"></form-builder-option>' +
           '<div class="form-group">' +
             '<label form-builder-tooltip="The width, offset, push and pull settings for the columns">{{\'Column Properties\' | formioTranslate}}</label>' +
