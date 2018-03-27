c589ddce43a75b2652b6bce8393d677ceeb7dd9b
Yury Rybak <Yury.Rybak@Softensity.com>
2018-03-27 17:26:30 +0300

    FOR-1139: Changed DateTime component FormBuilder tab and added 'moment' description.

--- src/components/panel.js
+++ src/components/panel.js
@@ -92,6 +92,7 @@ module.exports = function(app) {
               '<p>Enter custom conditional code.</p>' +
               '<p>You must assign the <strong>next</strong> variable with the API key of the next page.</p>' +
               '<p>The global variable <strong>data</strong> is provided, and allows you to access the data of any form component, by using its API key.</p>' +
+              '<p>Also <strong>moment</strong> library is available, and allows you to manipulate dates in a convenient way.</p>' +
             '</small>' +
           '</div>' +
           '<div uib-accordion-group heading="JSON Next Page" class="panel panel-default">' +
