7e307abe2fdf28f7dd4b1020c8472d2a893cf6a3
Travis Tidwell <travis@form.io>
2018-07-21 19:43:49 -0500

    Fixed the conditions for checking for data.id.

--- dist/ngFormBuilder.js
+++ dist/ngFormBuilder.js
@@ -33768,33 +33768,27 @@ module.exports = ['debounce', function(debounce) {
         }
 
         $scope.$on('iframe-componentUpdate', function(event, data) {
-          var hasId = data.hasOwnProperty('id');
           FormioUtils.eachComponent($scope.form.components, function(component) {
-            if (hasId) {
+            if (data.id) {
               if (component.id === data.id) {
                 component.overlay = data.overlay;
               }
             }
-            else {
-              if (component.key === data.key) {
-                component.overlay = data.overlay;
-              }
+            else if (component.key === data.key) {
+              component.overlay = data.overlay;
             }
           });
         });
 
         $scope.$on('iframe-componentClick', function(event, data) {
-          var hasId = data.hasOwnProperty('id');
           FormioUtils.eachComponent($scope.form.components, function(component) {
-            if (hasId) {
+            if (data.id) {
               if (component.id === data.id) {
                 $scope.$broadcast('editComponent', component);
               }
             }
-            else {
-              if (component.key === data.key) {
-                $scope.$broadcast('editComponent', component);
-              }
+            else if (component.key === data.key) {
+              $scope.$broadcast('editComponent', component);
             }
           });
         });
